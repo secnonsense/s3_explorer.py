2812d88662c602fc25d598abede3f9ff42655b52
secnonsense <27748027+secnonsense@users.noreply.github.com>
2025-05-16 21:45:28 -0400

    Update s3_explorer.py
    made credential file change to support Mac and Windows

--- s3_explorer.py
+++ s3_explorer.py
@@ -122,7 +122,7 @@ class S3ClientGUI:
 
 
     def _get_available_profiles(self):
-        config_path = os.path.expanduser("~/.aws/credentials")
+        config_path = os.path.join(os.path.expanduser('~'), '.aws', 'credentials')
         profiles = []
         if os.path.exists(config_path):
             with open(config_path, 'r') as f:
